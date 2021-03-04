539f93cbe46f0cf8ca6016d5aca215f9b372da5d
P99003664 <78850212+P99003664@users.noreply.github.com>
2021-03-04 19:40:20 +0530

    Create Shape.java

---
+++ Day3/Shape.java
@@ -0,0 +1,13 @@
+public abstract class Shape {
+	public int value;
+	
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+	abstract void calculateArea(int value);
+}
